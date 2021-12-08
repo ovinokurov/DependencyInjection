900986684c68ebb71f86b82aaadc4c5018e05f4b
Oleg Vinokurov <olegvinokurov@hotmail.com>
2021-12-07 18:13:13 -0800

    Add project files.

---
+++ Coupling/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Coupling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class UserInterface
+    {
+        public void GetData()
+        {
+            Console.Write("Enter your username: ");
+            var userName = Console.ReadLine();
+
+            Console.Write("Enter your password: ");
+            var password = Console.ReadLine();
+
+            IBusiness business = new BusinessV2();
+            business.SignUp(userName, password);
+        }
+    }
+
+    public class Business : IBusiness
+    {
+        public void SignUp(string userName, string password)
+        {
+            //validation
+            var dataAccess = new DataAccess();
+            dataAccess.Store(userName, password);
+        }
+    }
+
+    public class BusinessV2: IBusiness
+    {
+        public void SignUp(string userName, string password)
+        {
+            //validation
+            var dataAccess = new DataAccess();
+            dataAccess.Store(userName, password);
+        }
+    }
+    public class DataAccess : IDataAccess
+    {
+        public void Store(string userName, string password)
+        {
+            //write the data to the db
+        }
+    }
+
+    public interface IBusiness
+    {
+        void SignUp(string userName, string password);
+    }
+
+    public interface IDataAccess
+    {
+        void Store(string userName, string password);
+    }
+}
